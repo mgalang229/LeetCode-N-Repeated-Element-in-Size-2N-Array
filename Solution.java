94a8418f818df91da53d7f5e68696834c97a2fc4
jasonpogi <51401355+mgalang229@users.noreply.github.com>
2023-09-05 22:34:10 +0800

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int repeatedNTimes(int[] nums) {
+        int n = nums.length;
+        int repeat = n / 2;
+        Arrays.sort(nums);
+        int ans = -1;
+        for (int i = 0; i < n; i++) {
+            int index = i;
+            while (index < n && nums[index] == nums[i]) {
+                index++;
+            }
+            if (index - i == repeat) {
+                ans = nums[i];
+                break;
+            }
+        }
+        return ans;
+    }
+}
